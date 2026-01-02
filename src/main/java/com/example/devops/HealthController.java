df107e9dfa512c515258bc642864e23570838922
Rajeev <rajeevreddydevops1511@gmail.com>
2026-01-02 12:19:22 +0530

    Update health message - CI/CD test

--- src/main/java/com/example/devops/HealthController.java
+++ src/main/java/com/example/devops/HealthController.java
@@ -8,6 +8,6 @@ public class HealthController {
 
     @GetMapping("/health")
     public String health() {
-        return "Application is running";
+        return "Application is running through jenkins";
     }
 }
